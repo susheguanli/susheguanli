c18a9d9b5c56911a65e42cf144197ee7e320f888
kangjiaxu <kangjiaxu22@163.com>
2019-09-25 19:22:36 +0800

    所有宿舍管理系统需要的数据库表

---
+++ sushe.sql
@@ -0,0 +1,127 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.5.53 : Database - sushe
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sushe` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `sushe`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `Admin_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Admin_Username` varchar(20) DEFAULT NULL,
+  `Admin_Password` varchar(20) DEFAULT NULL,
+  `Admin_Name` varchar(20) DEFAULT NULL,
+  `Admin_Sex` varchar(10) DEFAULT NULL,
+  `Admin_Tel` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Admin_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `building` */
+
+DROP TABLE IF EXISTS `building`;
+
+CREATE TABLE `building` (
+  `Building_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Building_Name` varchar(50) DEFAULT NULL,
+  `Building_Introduction` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`Building_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `domitory` */
+
+DROP TABLE IF EXISTS `domitory`;
+
+CREATE TABLE `domitory` (
+  `Domitory_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Domitory_BuildingID` int(11) DEFAULT NULL,
+  `Domitory_Name` varchar(20) DEFAULT NULL,
+  `Domitory_Type` varchar(20) DEFAULT NULL,
+  `Domitory_Number` varchar(20) DEFAULT NULL,
+  `Domitory_Tel` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Domitory_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `log` */
+
+DROP TABLE IF EXISTS `log`;
+
+CREATE TABLE `log` (
+  `Log_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Log_StudentID` int(11) DEFAULT NULL,
+  `Log_TeacherID` int(11) DEFAULT NULL,
+  `Log_Date` varchar(50) DEFAULT NULL,
+  `Log_Remark` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`Log_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `out1` */
+
+DROP TABLE IF EXISTS `out1`;
+
+CREATE TABLE `out1` (
+  `Out_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Out_StudentID` varchar(11) DEFAULT NULL,
+  `Out_Date` varchar(50) DEFAULT NULL,
+  `Out_Remark` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`Out_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `Student_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Student_DomitoryID` int(11) DEFAULT NULL,
+  `Student_Username` varchar(20) DEFAULT NULL,
+  `Student_Password` varchar(20) DEFAULT NULL,
+  `Student_Name` varchar(20) DEFAULT NULL,
+  `Student_Sex` varchar(20) DEFAULT NULL,
+  `Student_Class` varchar(20) DEFAULT NULL,
+  `Student_State` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Student_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb` */
+
+DROP TABLE IF EXISTS `tb`;
+
+CREATE TABLE `tb` (
+  `TB_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `TB_TeacherID` int(11) DEFAULT NULL,
+  `TB_BuildingID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`TB_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `teacher` */
+
+DROP TABLE IF EXISTS `teacher`;
+
+CREATE TABLE `teacher` (
+  `Teacher_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Teacher_Username` varchar(20) DEFAULT NULL,
+  `Teacher_Password` varchar(20) DEFAULT NULL,
+  `Teacher_Name` varchar(20) DEFAULT NULL,
+  `Teacher_Sex` varchar(10) DEFAULT NULL,
+  `Teacher_Tel` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`Teacher_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
